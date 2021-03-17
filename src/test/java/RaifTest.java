1c8fe8ec7070b23574bd44c4271300dea5136ca0
deNNi <dns.nikonorov@gmail.com>
2021-03-17 20:05:20 +0400

    unused imports removed

--- src/test/java/RaifTest.java
+++ src/test/java/RaifTest.java
@@ -1,13 +1,16 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static java.util.Collections.addAll;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("listFormat function Test")
 public class RaifTest {
@@ -39,9 +42,7 @@ public class RaifTest {
     @Test
     void listContainsNullTest() {
         addAll(list, null, "Alice", "Joe", "bob", "alice", "dEN");
-        assertThrows(Exception.class, () -> {
-            page.listFormat(list);
-        }, "В списке присутствует null");
+        assertThrows(Exception.class, () -> page.listFormat(list), "В списке присутствует null");
     }
 
     @DisplayName("Given list with not Latin value " +
@@ -50,9 +51,7 @@ public class RaifTest {
     @Test
     void listContainsLatinTest() {
         addAll(list, "фываыв", "Alice", "Joe", "bob", "alice", "dEN");
-        assertThrows(Exception.class, () -> {
-            page.listFormat(list);
-        }, "В списке должны присутсвовать только латинские буквы");
+        assertThrows(Exception.class, () -> page.listFormat(list), "В списке должны присутсвовать только латинские буквы");
     }
 
     @DisplayName("Given list with Latin letters with 3 repeated values " +
